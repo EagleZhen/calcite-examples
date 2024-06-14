9194f8cd78fc7825e26dc2beeafc75ba0f28c471
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-06-14 11:45:45 +0800

    add comments to help me understand

--- src/main/java/hr/fer/zemris/calcite/sqlparser/SQLParse.java
+++ src/main/java/hr/fer/zemris/calcite/sqlparser/SQLParse.java
@@ -16,15 +16,15 @@ import org.apache.calcite.sql.validate.SqlConformanceEnum;
  */
 public class SQLParse {
     public static void main(String[] args) throws SqlParseException {
-        SqlParser sqlParser = SqlParser.create(new SourceStringReader(args[0]),
-                SqlParser.configBuilder()
+        SqlParser sqlParser = SqlParser.create(new SourceStringReader(args[0]), // Reads the SQL query from the first command-line argument
+                SqlParser.configBuilder() // Configures the parser
                         .setParserFactory(SqlParserImpl.FACTORY)
-                        .setQuoting(Quoting.DOUBLE_QUOTE)
+                        .setQuoting(Quoting.DOUBLE_QUOTE) // Sets the quoting to double quotes, i.e. identifiers (such as table and column names) can be quoted using double quotes.
                         .setUnquotedCasing(Casing.TO_UPPER)
                         .setQuotedCasing(Casing.UNCHANGED)
                         .setConformance(SqlConformanceEnum.DEFAULT)
                         .build());
-        SqlNode sqlNode = sqlParser.parseQuery();
+        SqlNode sqlNode = sqlParser.parseQuery(); // This would generate an abstract syntax tree (AST) for the SQL query, could set a breakpoint here and use a debugger to check the values of the nodes in the AST
         System.out.println(sqlNode.toString());
     }
 }
